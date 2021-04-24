1e7efb5e52512baa4f01ad04efc49f95ddd564d0
root <root@mithunmohanangm.opoxndtcibkepggp4kz525x5rd.gx.internal.cloudapp.net>
2021-04-24 04:10:00 +0000

    adding 3 files to commit

--- first.java
+++ first.java
@@ -2,6 +2,6 @@ class Hello{
 
 public static void main(String args[]{
 
-System.out.printl("First Java added to .git ignore");
+System.out.printl("First Java added to .git ignore second attempt");
 	}
 }
--- second.java
+++ second.java
@@ -2,6 +2,6 @@ class Hello{
 
 public static void main(String args[]{
 
-System.out.printl("Second Java changed");
+System.out.printl("Second Java changed second attempt");
 	}
 }
